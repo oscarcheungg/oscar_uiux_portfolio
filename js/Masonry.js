6a532542f7214cfba4e5a3d87a1d4b378b29ceb2
oscarcheung <oscarcheung2023@gmail.com>
2025-07-09 15:23:41 -0400

    Minor Change

--- js/Masonry.js
+++ js/Masonry.js
@@ -111,24 +111,6 @@ class Masonry {
       }
 
       this.container.appendChild(wrapper);
-
-      // Animate from bottom
-      if (window.gsap) {
-        gsap.fromTo(
-          wrapper,
-          {
-            opacity: 0,
-            y: 100,
-          },
-          {
-            opacity: 1,
-            y: 0,
-            duration: 1.5,
-            ease: "power3.out",
-            delay: 0.12 * idx,
-          }
-        );
-      }
     });
   }
 } 
